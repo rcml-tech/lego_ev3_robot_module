1e29bb616348741c46d3bb66c7959fbc77bb6a08
Mikhail Myasnikov <myasnikov.mike@gmail.com>
2015-03-31 18:23:30 +0400

    Update lego_module.h

--- LegoRobotModule/lego_module.h
+++ LegoRobotModule/lego_module.h
@@ -12,39 +12,39 @@ public:
 	int robot_index; 
 
 	bool is_locked; 
-	std::vector<regval> axis_state;
+	std::vector<regval> axis_state; 
 
 	LegoRobot(int robot_index) : robot_index(robot_index), isAviable(true) {} 
 
 	FunctionResult* executeFunction(regval command_index, regval *args);
-	void axisControl(regval axis_index, regval value);  
+	void axisControl(regval axis_index, regval value); 
 	~LegoRobot() {};
 };
 
-typedef std::map<std::string, LegoRobot*> m_connections; 
+typedef std::map<std::string, LegoRobot*> m_connections; // Таким образом создадим карту которая строке из config.ini в соответствие ставит адрес объекта LegoRobot. 
 
 
 class LegoRobotModule : public RobotModule{
 public:
-	CRITICAL_SECTION LRM_cs; 
+	CRITICAL_SECTION LRM_cs; // Критическая секция для LegoRobotModule
 	m_connections aviable_connections; 
 	FunctionData **lego_functions;
 	AxisData **robot_axis; 
 	colorPrintf_t *colorPrintf;
+	LegoRobot *OccRobot;
+
+	LegoRobotModule();
 
 	const char *getUID();
+	void prepare(colorPrintf_t *colorPrintf_p, colorPrintfVA_t *colorPrintfVA_p);
 	int init();
 	FunctionData** getFunctions(int *count_functions);
 	AxisData** getAxis(int *count_axis) ;
 	Robot* robotRequire() ;
-	void prepare(colorPrintf_t *colorPrintf_p, colorPrintfVA_t *colorPrintfVA_p);
 	void robotFree(Robot *robot) ;
 	void final();
 	void destroy() ;
-	LegoRobotModule();
 	~LegoRobotModule() {};
 };
-
-
 
 #endif	/* LEGO_MODULE_H */
