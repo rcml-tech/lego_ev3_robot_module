a86e80bf33c70181fec0c4dd2de76352484dd571
Mikhail Myasnikov <myasnikov.mike@gmail.com>
2015-04-03 17:58:52 +0400

    Update Program.cs
    Добавлена функция readSensor и протектед функции для ее работы: getSensorObject и resultMod для можификации данных датчика

--- lego_communication_library/Program.cs
+++ lego_communication_library/Program.cs
@@ -294,10 +294,186 @@ namespace lego_communication_library
             Brick<Sensor, Sensor, Sensor, Sensor> brick = getBrickByIndex(indexBrick); // Записали в переменную типа Brick наш созданный brick
             //brick.Sensor1 = new UltrasonicSensor(UltrasonicMode.Centimeter);
             brick.Sensor1 = new IRSensor(IRMode.Proximity);  // Создаем в нашем Brick сенсор. Это свойство/поле Sensor1.  Создаем объект Инфракрасный сенсор.
-            Console.WriteLine(brick.Sensor1.GetSensorType());
+            Console.WriteLine(brick.Sensor1.ReadAsString());
             brick.Sensor2 = new UltrasonicSensor(UltrasonicMode.Centimeter);
-            Console.WriteLine(brick.Sensor2.GetSensorType());
+            Console.WriteLine(brick.Sensor2.ReadAsString());
+            brick.Sensor3 = new GyroSensor(GyroMode.Angle);
+            Console.WriteLine(brick.Sensor3.ReadAsString());
+            brick.Sensor3 = new GyroSensor(GyroMode.AngularVelocity);
+            Console.WriteLine(brick.Sensor3.ReadAsString());
+            brick.Sensor4 = new ColorSensor(ColorMode.Color);
+            Console.WriteLine(brick.Sensor4.ReadAsString());
+            brick.Sensor4 = new ColorSensor(ColorMode.Raw);
+            Console.WriteLine(brick.Sensor4.ReadAsString());
+            brick.Sensor4 = new ColorSensor(ColorMode.Reflection);
+            Console.WriteLine(brick.Sensor4.ReadAsString());
             // Определяем тип сенсора 1
+
+             Brick<Sensor, Sensor, Sensor, Sensor> brick2 = getBrickByIndex(indexBrick);
+
+             switch ( Convert.ToString( brick2.Sensor2.GetSensorType() ) )
+             {
+                 case "UltraSonic": 
+                    {
+                        Console.WriteLine("UltraSonic");
+
+                        brick2.Sensor2 = new UltrasonicSensor(UltrasonicMode.Centimeter);
+                        string temps;
+                        temps = Convert.ToString( brick2.Sensor2.ReadAsString() ) ;
+                        double tempi;
+                        int tsi = temps.IndexOf('c');
+                        temps = temps.Remove(tsi-1,3);
+                        //Console.WriteLine(temps.IndexOf('c'));
+                        Console.WriteLine(temps);
+                        tempi = Convert.ToDouble(temps);
+                        Console.WriteLine(tempi);
+
+                        break;
+                    }
+                 case "IR": 
+                    {
+                        Console.WriteLine("IR");
+                        brick2.Sensor2 = new UltrasonicSensor(UltrasonicMode.Centimeter);
+                        
+
+
+                        break;
+                    }
+                 case "Color": 
+                    {
+                        Console.WriteLine("Color");
+                        break;
+                    }
+                 case "Gyro":
+                    {
+                        Console.WriteLine("Gyro");
+                        break;
+                    }
+
+             };
+
+
+        } // End ReadIRSensor
+
+
+        private Sensor getSensorObject(SensorType typeIndexSensor, int mode) 
+        {
+            switch (typeIndexSensor) {
+                case SensorType.UltraSonic:
+                    {
+                        switch (mode) {
+                            case 1: { return new UltrasonicSensor(UltrasonicMode.Centimeter); }
+                            case 2: { return new UltrasonicSensor(UltrasonicMode.Inch); }
+                            case 3: { return new UltrasonicSensor(UltrasonicMode.Listen); }
+                            default: { return null; };
+                        }
+                    }
+                case SensorType.IR:
+                    {
+                        switch (mode)
+                        {
+                            case 1: { return new IRSensor(IRMode.Proximity); }
+                            case 2: { return new IRSensor(IRMode.Remote); }
+                            case 3: { return new IRSensor(IRMode.Seek); }
+                            default: { return null; };  
+                        }
+                    }
+                case SensorType.Color:
+                    {
+                        switch (mode)
+                        {
+                            case 1: { return new ColorSensor(ColorMode.Ambient); }
+                            case 2: { return new ColorSensor(ColorMode.Color); }
+                            case 3: { return new ColorSensor(ColorMode.Raw); }
+                            default: { return null; };
+                        }
+                    }
+                case SensorType.Gyro:
+                    {
+                        switch (mode)
+                        {
+                            case 1: { return new GyroSensor(GyroMode.Angle); }
+                            case 2: { return new GyroSensor(GyroMode.AngularVelocity); }
+                            case 3: { return new GyroSensor(GyroMode.Angle); }
+                            default: { return null; };
+
+                        }
+                       
+                    }
+                case SensorType.Touch:
+                    {
+                        switch (mode)
+                        {
+                            case 1: { return new TouchSensor(TouchMode.Boolean); }
+                            case 2: { return new TouchSensor(TouchMode.Count); }
+                            case 3: { return new TouchSensor(TouchMode.Boolean); }
+                            default: { return null; };
+                        }
+                    }
+                default: { return null; };
+            }; // End Switch
+        } // End Func
+
+
+        protected double resultMod(string str) {
+            switch (str) {
+                case "Black": { return 1; }
+                case "White": { return 2; }
+                case "Blue": { return 3; }
+                case "Green": { return 4; }
+                case "Yellow": { return 5; }
+                case "Red": { return 6; }
+                default:
+                    {
+                        string temps;
+                        temps = str;
+                        int tempi = 0;
+
+                        tempi = temps.Length;
+
+                        if (temps.IndexOf(' ') > 0)
+                        {
+                            temps = temps.Remove(temps.IndexOf(' '), tempi - temps.IndexOf(' '));
+                        }
+                        return Convert.ToDouble(temps);
+                    }
+            }
         }
+
+        public double readSensor(int indexBrick, int indexSensor, int mode)
+        {
+            Brick<Sensor, Sensor, Sensor, Sensor> brick2 = getBrickByIndex(indexBrick);
+            double ret = 0;
+
+            switch (indexSensor)
+            {
+                case 1:
+                    {   
+                        brick2.Sensor1 = getSensorObject(brick2.Sensor1.GetSensorType(), mode);
+                        ret = resultMod(brick2.Sensor1.ReadAsString());
+                        break;
+                    }
+                case 2: 
+                    {
+                        brick2.Sensor2 = getSensorObject(brick2.Sensor2.GetSensorType(), mode);
+                        ret = resultMod(brick2.Sensor2.ReadAsString());
+                        break;
+                    }
+                case 3: 
+                    {
+                        brick2.Sensor3 = getSensorObject(brick2.Sensor3.GetSensorType(), mode);
+                        ret = resultMod(brick2.Sensor3.ReadAsString());
+                        break;
+                    }
+                case 4: 
+                    {
+                        brick2.Sensor4 = getSensorObject(brick2.Sensor4.GetSensorType(), mode);
+                        ret = resultMod(brick2.Sensor4.ReadAsString());
+                        break;
+                    }
+                default: { break; }
+            };
+            return ret;
+        } // End readSensor
     }
 }
