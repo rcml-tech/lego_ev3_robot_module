4ee9be2d97d5f779139bd3abad1b2810c8f94e43
Mikhail Myasnikov <myasnikov.mike@gmail.com>
2015-06-11 17:17:31 +0500

    Подготовлен для компиляции в Windows

--- lego_ev3_robot_module.cpp
+++ lego_ev3_robot_module.cpp
@@ -207,14 +207,10 @@ int LegoRobotModule::init(){
 	CSimpleIniA ini;
 	ini.SetMultiKey(true);
 
-	HMODULE lr_handle;
-
-	lr_handle = GetModuleHandleW(L"lego_ev3_module.dll");
-
 	WCHAR DllPath[MAX_PATH] = { 0 };
 	
-	//GetModuleFileNameW((HINSTANCE)&__ImageBase, DllPath, _countof(DllPath));
-	GetModuleFileNameW(lr_handle, DllPath, _countof(DllPath));
+	GetModuleFileNameW((HINSTANCE)&__ImageBase, DllPath, _countof(DllPath));
+
 	WCHAR *tmp = wcsrchr(DllPath, L'\\');
 	WCHAR ConfigPath[MAX_PATH] = { 0 };
 	size_t path_len = tmp - DllPath;
@@ -225,7 +221,6 @@ int LegoRobotModule::init(){
 		printf("Can't load '%s' file!\n", ConfigPath);
 		return 1;
 	}
-
 
 	allow_dynamic = ini.GetBoolValue("options", "dynamic_connection", false);
 
